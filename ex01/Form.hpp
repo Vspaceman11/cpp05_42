56dab3f8a5dbdb4827090c7ac0c43956b48e13ba
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2026-04-08 14:25:08 +0200

    Init Form class

---
+++ ex01/Form.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <exception>
+class Bureaucrat;
+
+class Form
+{
+	public:
+		Form();
+		Form(const std::string& name, int gradeToSign, int gradeToExecute);
+		Form(const Form& other);
+		Form& operator=(const Form& other);
+		~Form();
+
+		const std::string	getName() const;
+		bool				getIsSigned() const;
+		const int			getGradeToSign() const;
+		const int			getGradeToExecute() const;
+		void				beSigned(const Bureaucrat& b);
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const noexcept;
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const noexcept;
+		};
+
+	private:
+		const std::string	_name;
+		bool				_isSigned;
+		const int			_gradeToSign;
+		const int			_gradeToExecute;
+};
+
+std::ostream& operator<<(std::ostream& os, const Form& form);
